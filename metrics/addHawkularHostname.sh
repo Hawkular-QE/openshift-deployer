73e99febcbc9ca8a778ee4f39c3717d46f7642d8
Matt Mahoney <mmahoney@redhat.com>
2015-12-02 14:35:05 -0500

    Key off of metricsPublicURL instead URL

--- metrics/addHawkularHostname.sh
+++ metrics/addHawkularHostname.sh
@@ -12,12 +12,12 @@ PUBLIC_URL="\ \ metricsPublicURL: ${METRICS_PUBLIC_URL}"
 # Backup the masetr-config prior to modifying
 cp ${OPENSHIFT_MASTER_CONFIG} ${OPENSHIFT_MASTER_CONFIG}-`date +%s`
 
-grep ${METRICS_PUBLIC_URL} ${OPENSHIFT_MASTER_CONFIG}
+grep metricsPublicURL ${OPENSHIFT_MASTER_CONFIG}
 if [ $? -eq 0 ]; then
     ## Remove the existing metricsPublicURL, and add new value
 
     LINE=`awk -v var=metricsPublicURL 'match($0, var){print NR; exit}' ${OPENSHIFT_MASTER_CONFIG}`
-    echo "Deleting \"${METRICS_PUBLIC_URL}\" from \"${OPENSHIFT_MASTER_CONFIG}\"."
+    echo "Deleting \"metricsPublicURL\" from \"${OPENSHIFT_MASTER_CONFIG}\"."
     sed -i -e "$LINE"'d' ${OPENSHIFT_MASTER_CONFIG}
 
     echo "Adding \"${PUBLIC_URL=}\" to \"${OPENSHIFT_MASTER_CONFIG}\"."
